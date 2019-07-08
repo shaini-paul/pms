ef69488dd344b55347b1b538b7d1e5aead7dbbd7
shaini-paul <39427494+shaini-paul@users.noreply.github.com>
2019-07-08 19:05:50 +0530

    Create themes.php

---
+++ themes.php
@@ -0,0 +1,10 @@
+Gktechnoindia
+https://www.vwthemes.com/themes/free-it-company-wordpress-theme/
+http://themedemo.web-dorado.com/sauron/
+https://wordpress.org/themes/company-elite/
+https://wordpress.org/themes/corporate-gravity/
+https://wordpress.org/themes/compact-one/
+https://demo.aarambhathemes.com/business-plan/
+https://colorlib.com/shapely/
+https://preview.rigorousthemes.com/political-era/default/
+https://organicthemes.com/demo/?demo=profile-theme
